961778a98788773ffe64805955c8b07a9454266d
alex-sly <80974804+alex-sly@users.noreply.github.com>
2021-04-09 22:11:58 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    }
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
     prevEl: '.channel-button-prev',
   },
+});
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
 });
